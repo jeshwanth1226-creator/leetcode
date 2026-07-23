36da4a1f9a7275ceb9ba30bc10e8049af8621088
jeshwanth1226-creator <gannojujeshwanth1226@gmail.com>
2026-07-23 13:02:27 +0530

    Time: 0 ms (100%), Space: 13.4 MB (35.58%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def search(self, nums, target):
+        start=0
+        end=len(nums)-1
+        while(start<=end):
+            mid=(start+end)//2
+            if target<nums[mid]:
+                end=mid-1
+            elif target>nums[mid]:
+                start=mid+1
+            else:
+                return mid
+        return -1
+
+
+        
