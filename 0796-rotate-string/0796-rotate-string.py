467fa449700eb21e7e7d0a4448339c086eac8ebe
jeshwanth1226-creator <gannojujeshwanth1226@gmail.com>
2026-08-04 23:20:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (52.42%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        """
+        :type s: str
+        :type goal: str
+        :rtype: bool
+        """
+        if len(s)!=len(goal):
+            return False
+            
+        if goal in (s+s):
+            return True
+        else:
+            return False
+        
