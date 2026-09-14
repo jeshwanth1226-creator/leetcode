c6ee0be9086cd66312493ff6aff95ba6a1279c5c
Jeshwanth <gannojujeshwanth1226@gmail.com>
2026-09-14 20:54:31 +0530

    Time: 159 ms (77.25%), Space: 13.6 MB (93.89%) - LeetHub

---
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -0,0 +1,40 @@
+class Solution(object):
+    def subarraysWithKDistinct(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        return self.atmost(nums,k)-self.atmost(nums,k-1)
+
+    def atmost(self,nums,k):
+        left=0
+        count=0
+        freq={}
+        
+
+        for right in range(len(nums)):
+
+                freq[nums[right]]=freq.get(nums[right],0)+1
+                
+                while len(freq)>k:
+
+                    freq[nums[left]]-=1
+
+                    if freq[nums[left]]==0:
+
+                        del freq[nums[left]]
+                    
+                    left+=1
+                
+                count+=right-left+1
+               
+        return count
+
+            
+
+            
+
+
+                
+        
